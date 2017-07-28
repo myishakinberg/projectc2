1b62e5a7bf13977356588afa2f79b3850b5af31a
Myisha Kinberg <myishakinberg@Myishas-MacBook-Pro.local>
2017-07-27 17:01:13 -0700

    A lot has been added for the hair donations page

--- sites/all/modules/custom_map/js/map.js
+++ sites/all/modules/custom_map/js/map.js
@@ -1,13 +1,9 @@
-window.onload = getLocation;
+window.onload = tryAPIGeolocation;
 
-function getLocation() {
-    navigator.geolocation.getCurrentPosition(sendPosition);
-}
 
 function sendPosition(position) {
     var act_lat =  position.coords.latitude;
     var act_lon = position.coords.longitude;
-
     var lat = document.getElementsByName("field_test_part_2[0][value][lat]");
     lat.item(0).value = act_lat;
 
@@ -15,3 +11,9 @@ function sendPosition(position) {
     lon.item(0).value = act_lon;
 
 }
+
+function tryAPIGeolocation() {
+    jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCgQtF_rKAVSQzyR30KebWYi6VrDhCrcW4", function(success) {
+        sendPosition({coords: {latitude: success.location.lat, longitude: success.location.lng}});
+    });
+};
